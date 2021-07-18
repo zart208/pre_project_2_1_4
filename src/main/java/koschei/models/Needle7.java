527f349f1e9c288639744f109d5f0046b3196f22
Artem Zakharov <zart208@gmail.com>
2021-07-18 10:55:33 +0300

    My decision commit

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,6 +1,16 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Needle7 {
+    private Deth8 deth8;
+
+    @Autowired
+    public void setDeth8(Deth8 deth8) {
+        this.deth8 = deth8;
+    }
 
     @Override
     public String toString() {
--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,19 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
+    private Duck5 duck5;
+
+    @Autowired
+    public Rabbit4(Duck5 duck5) {
+        this.duck5 = duck5;
+    }
 
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + duck5.toString();
     }
 }
